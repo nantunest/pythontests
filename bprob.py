41398ddd67c918b49390d88978fc333a39db2afc
Nelson Terra <na.terra@gmail.com>
2017-09-14 17:50:19 -0300

    additions to bprob

--- bprob.py
+++ bprob.py
@@ -1,6 +1,25 @@
 import random
 
-crd = [[random.sample(xrange(1,15) for i in xrange(5)] for i in xrange(100)]
+cards = [[random.sample(xrange(1,15), 5) for i in xrange(5)] for i in xrange(5)]
+matchcards = [[False for i in xrange(5)] for j in xrange(5)]
 
-nums = range(1,81)
+nums = range(1,5)
 
+print "Before"
+print cards
+
+random.shuffle(nums)
+
+print "Numbers"
+print nums
+
+for i in nums:
+    
+    for icard in cards:
+        for icol in icard:
+            for inum in icol:
+                if inum == i:
+                    print inum, i
+                    
+print "After"
+print cards
